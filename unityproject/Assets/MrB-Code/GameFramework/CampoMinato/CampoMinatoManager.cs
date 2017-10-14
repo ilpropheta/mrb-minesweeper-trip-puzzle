b6b1807413289933c9cb5569607ef4386c7e7a8e
ilpropheta <marcoarena@email.it>
2017-10-14 20:34:33 +0200

    qualcosa si muove

--- unityproject/Assets/MrB-Code/GameFramework/CampoMinato/CampoMinatoManager.cs
+++ unityproject/Assets/MrB-Code/GameFramework/CampoMinato/CampoMinatoManager.cs
@@ -5,10 +5,22 @@ using System.Text.RegularExpressions;
 
 public class CampoMinatoManager : MonoBehaviour
 {
+    static readonly int ROWS = 8;
+    static readonly int COLS = 17;
+
     // 8x17 (with holes)
     public GameObject matrixRoot;
     Minesweeper.MinesweeperGraph graph = new Minesweeper.MinesweeperGraph();
-    Minesweeper.MinesweeperNode[][] matrix = new Minesweeper.MinesweeperNode[8][];
+    Minesweeper.MinesweeperNode[][] matrix = new Minesweeper.MinesweeperNode[ROWS][];
+    Button[][] buttonsMatrix = new Button[ROWS][];
+
+    public Image btn_bomb;
+    public Image btn_empty;
+    public Image btn_flag;
+    public Image btn_one;
+    public Image btn_two;
+    public Image btn_three;
+    public Image btn_four;
 
     void AddConnection(int from_I, int from_J, int to_I, int to_J)
     {
@@ -16,11 +28,33 @@ public class CampoMinatoManager : MonoBehaviour
         //Debug.Log(string.Format("Connection: {0} {1} - {2} {3}", from_I, from_J, to_I, to_J));
     }
 
+    Sprite NumberOfBombsToSprite(Minesweeper.MinesweeperNode node)
+    {
+        if (node.nearMines == 1)
+        {
+            return btn_one.sprite;
+        }
+        if (node.nearMines == 2)
+        {
+            return btn_two.sprite;
+        }
+        if (node.nearMines == 3)
+        {
+            return btn_three.sprite;
+        }
+        if (node.nearMines == 4)
+        {
+            return btn_four.sprite;
+        }
+        return null;
+    }
+
     void Start ()
     {
         for(var i=0; i<matrix.Length; ++i)
         {
-            matrix[i] = new Minesweeper.MinesweeperNode[17];
+            matrix[i] = new Minesweeper.MinesweeperNode[COLS];
+            buttonsMatrix[i] = new Button[COLS];
         }
 
         string pattern = @"^Button_(\d+)_(\d+)$";
@@ -42,10 +76,12 @@ public class CampoMinatoManager : MonoBehaviour
             }
 
             matrix[row][col] = graphNode;
+            buttonsMatrix[row][col] = button;
+
             //Debug.Log(string.Format("{0} {1}", row, col));
             button.onClick.AddListener(() =>
             {
-                OnButtonClick(graphNode);
+                OnButtonClick(button, graphNode);
             });
         }
 
@@ -93,12 +129,43 @@ public class CampoMinatoManager : MonoBehaviour
                 
             }
         }
+
+        graph.ComputeNearMines();
 	}
 	
-    void OnButtonClick(Minesweeper.MinesweeperNode graphNode)
+    void OnButtonClick(Button uiButton, Minesweeper.MinesweeperNode graphNode)
     {
         Debug.Log(string.Format("clicked on {0}", graphNode.label));
         graph.UncoverSafeNodes(graphNode);
+        uiButton.gameObject.GetComponent<Image>().sprite = btn_empty.sprite;
+
+        if (graphNode.mined)
+        {
+            uiButton.gameObject.GetComponent<Image>().sprite = btn_bomb.sprite;
+            // sconfitta
+            return;
+        }
+
+        for (var i=0; i<ROWS; ++i)
+        {
+            for (var j=0; j<COLS; ++j)
+            {
+                if (matrix[i][j] != null)
+                {
+                    // è stato scoperto?
+                    if (!matrix[i][j].covered)
+                    {
+                        Debug.Log(string.Format("{0} {1} scoperto", i, j));
+                        buttonsMatrix[i][j].gameObject.GetComponent<Image>().sprite = btn_empty.sprite;
+                    }
+                    if (matrix[i][j].nearMines > 0)
+                    {
+                        Debug.Log(string.Format("{0} {1} ha {2}", i, j, matrix[i][j].nearMines));
+                        buttonsMatrix[i][j].gameObject.GetComponent<Image>().sprite = NumberOfBombsToSprite(matrix[i][j]);
+                    }
+                }
+            }
+        }
     }
 
 	
